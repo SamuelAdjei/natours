99b7595a75c076486f9513fc0e08ff55276d26ea
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-03-19 23:06:27 +0000

    salt hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,8 +22,26 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please provide your password']
+    required: [true, 'Please provide your password'],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function(el) {
+        return el === this.password;
+      }
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hashing password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
