41fd32cdbc647d8f9b9f2bf88046d76b05023b07
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-03-21 22:50:53 +0000

    signup completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,38 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1. If email and password exist
+  if (!email || !password) {
+    new AppError();
+  }
+
+  // 2. Check if user && password are correct
+
+  // 3. If everything is okay, send toke to client
+};
