3f5c8b2d0bd49aea87bc603f61ca1d84ee0155ac
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-04-04 22:09:22 +0100

    updated forgotten password

--- models/userModel.js
+++ models/userModel.js
@@ -54,6 +54,13 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 // confirm that the hash password === user password
 userSchema.methods.correctPassword = async function(
   candidatePassword,
