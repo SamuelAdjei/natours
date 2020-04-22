d1eaf2e6a5d09c2876835c3a7d0b7eff379a377d
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-04-22 01:15:22 +0100

    new review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,1 +1,38 @@
 const mongoose = require('mongoose');
+
+const reviewSchema = new momgoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review cannot be empty! ']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A review must belong to a tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must belong to a user']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
