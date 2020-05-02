89210a7bfb8162aa4d145bd8f576d003f66520bf
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-05-03 00:33:54 +0100

    updated factpry handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,19 +2,20 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
+exports.getAllReviews = factory.getAll(Review);
+// exports.getAllReviews = catchAsync(async (req, res, next) => {
+//   let filter = {};
+//   if (req.params.tourId) filter = { tour: req.params.tourId };
 
-  const reviews = await Review.find(filter);
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
+//   const reviews = await Review.find(filter);
+//   res.status(200).json({
+//     status: 'success',
+//     results: reviews.length,
+//     data: {
+//       reviews
+//     }
+//   });
+// });
 
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
@@ -22,6 +23,7 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
--- controllers/userController.js
+++ controllers/userController.js
@@ -10,19 +10,24 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
+exports.getAllUsers = factory.getAll(User);
+// exports.getAllUsers = catchAsync(async (req, res, next) => {
+//   const users = await User.find();
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
+//   // SEND RESPONSE
+//   res.status(200).json({
+//     status: 'success',
+//     results: users.length,
+//     data: {
+//       users
+//     }
+//   });
+// });
 
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1. Create error if user POSTs password data
@@ -60,13 +65,14 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
+exports.getUser = factory.getOne(User);
+
+exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined! Please use /signup instead'
   });
 };
-exports.createUser = factory.createOne(User);
 
 // Do not update passwords with this
 exports.updateUser = factory.updateOne(User);
