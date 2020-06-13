5e7cbcd324b971547c75a4cb960a929959cccc71
Samuel Adjei <samueladjei@Samuels-MacBook-Pro.local>
2020-06-13 04:16:39 +0100

    new login screen (nonfunctional)

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the Data for the requested tour (reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -28,3 +28,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your Account'
+  });
+};
